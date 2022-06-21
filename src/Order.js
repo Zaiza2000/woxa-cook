7a1aecd2610c03496036efd7de509586ab134e49
Zaiza2000 <69431042+Zaiza2000@users.noreply.github.com>
2022-06-21 17:34:08 +0700

    Implement Table

--- src/Order.js
+++ src/Order.js
@@ -1,6 +1,6 @@
 import { db } from "./firebase";
 import { set, ref, onValue, remove, update } from "firebase/database";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Order() {
     const [order, setOrder] = useState("");
@@ -62,53 +62,33 @@ export default function Order() {
     var orderNo = 0;
 
     return (
-
         <section className="overflow-x-auto">
             <h1 className="m-10 text-lg font-bold">WOXA KITCHEN</h1>
-            {/* <div>
-        {orderArray.map((order) => (
-          <>
-            <h1 className="m-5"> อาหารที่คุณสั่งคือ {order.order}</h1>
-          </>
-        ))}
-      </div> */}
-
-
             <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
                 <table className="w-full text-sm text-left text-gray-500 ">
                     <thead className="text-sm text-gray-700 uppercase bg-gray-50 ">
                         <tr>
-                            <th scope="col" className="px-6 py-3">
-                                คิวที่
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                ชื่อผู้สั่ง
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                เมนูที่สั่ง
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                สถานะ
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                <span className="sr-only">Edit</span>
-                            </th>
+                            <th scope="col" className="px-6 py-3">คิวที่</th>
+                            <th scope="col" className="px-6 py-3">ชื่อผู้สั่ง</th>
+                            <th scope="col" className="px-6 py-3">เมนูที่สั่ง</th>
+                            <th scope="col" className="px-6 py-3">สถานะ</th>
                         </tr>
                     </thead>
-                    {orderArray.map((order) => (
-                        <tr className="bg-white border-b  hover:bg-gray-50">
-                            <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">{++orderNo}</td>
-                            <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"> {order.name}</td>
-                            <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">{order.order}</td>
-                            <button className="font-medium py-2 px-5 bg-green-500 hover:bg-green-400 text-white rounded-lg mt-2 mr-10">เสร็จแล้ว</button>
-                            <button className="font-medium py-2 px-5 bg-red-600 hover:bg-red-400 text-white rounded-lg mr-10">ไม่มีเมนูนี้</button>
-                        </tr>
-                    ))}
+                    <tbody>
+                        {orderArray.map((order) => (
+                            <tr className="bg-white border-b hover:bg-gray-50">
+                                <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">{++orderNo}</td>
+                                <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"> {order.name}</td>
+                                <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">{order.order}</td>
+                                <td className="font-medium ">
+                                    <button className="py-2 px-5 bg-green-500 hover:bg-green-400 text-white rounded-lg mt-2 mr-10">เสร็จแล้ว</button>
+                                    <button className="py-2 px-5 bg-red-600 hover:bg-red-400 text-white rounded-lg mr-10">ไม่มีเมนูนี้</button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
                 </table>
             </div>
-
-
         </section>
-
     );
 }
