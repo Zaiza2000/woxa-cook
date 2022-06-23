c5cab25d143455e41b3a1cc7626b32099950df75
Thanet-Bus <76712551+Thanet-Bus@users.noreply.github.com>
2022-06-23 11:26:02 +0700

    Add send message function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { db } from "./firebase";
 import { set, ref, onValue, remove, update } from "firebase/database";
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function App() {
   const [order, setOrder] = useState("");
   const [orderArray, setOrderArray] = useState([]);
   const [isEdit, setIsEdit] = useState(false);
   const [tempId, setTempId] = useState("");
-  const [showStatus, setShowstatus] = useState('');
+  const [showStatus, setShowstatus] = useState("");
   const handleOrderChange = (e) => {
     setOrder(e.target.value);
   };
@@ -24,6 +25,28 @@ export default function App() {
       }
     });
   }, []);
+
+  async function finishedOrder(userId, order) {
+    try {
+      await axios.post("https://woxa-food-order.herokuapp.com/finished-order", {
+        id: userId,
+        order: order,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function rejectOrder(userId, order) {
+    try {
+      await axios.post("https://woxa-food-order.herokuapp.com/reject-order", {
+        id: userId,
+        order: order,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   // //write
   // const writeToDatabase = () => {
@@ -64,7 +87,7 @@ export default function App() {
   //showstatus
   const toggleShowStatus = () => {
     setShowstatus(!showStatus);
-  }
+  };
   return (
     <section className="overflow-x-auto">
       <h1 className="m-10 text-lg text-center font-bold">WOXA KITCHEN</h1>
@@ -72,32 +95,62 @@ export default function App() {
         <table className="w-full text-sm text-left text-gray-500 ">
           <thead className="text-sm text-white uppercase bg-gray-800">
             <tr>
-              <th scope="col" className="px-6 py-3">คิวที่</th>
-              <th scope="col" className="px-6 py-3">ชื่อผู้สั่ง</th>
-              <th scope="col" className="px-6 py-3">เมนูที่สั่ง</th>
-              <th scope="col" className="px-6 py-3">ตรวจสอบ</th>
-              <th scope="col" className="px-6 py-3">สถานะ</th>
-
+              <th scope="col" className="px-6 py-3">
+                คิวที่
+              </th>
+              <th scope="col" className="px-6 py-3">
+                ชื่อผู้สั่ง
+              </th>
+              <th scope="col" className="px-6 py-3">
+                เมนูที่สั่ง
+              </th>
+              <th scope="col" className="px-6 py-3">
+                ตรวจสอบ
+              </th>
+              <th scope="col" className="px-6 py-3">
+                สถานะ
+              </th>
             </tr>
           </thead>
-          <tbody >
+          <tbody>
             {orderArray.map((order, index) => (
               <tr className="bg-white border-b hover:bg-gray-50" key={index}>
-                <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">{++orderNo}</td>
-                <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"> {order.name}</td>
-                <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap" >{order.order}</td>
+                <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                  {++orderNo}
+                </td>
+                <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                  {" "}
+                  {order.name}
+                </td>
+                <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                  {order.order}
+                </td>
                 <td className="font-medium ">
-                  <button className="py-2 px-5 bg-green-500 hover:bg-green-400 text-white rounded-lg mt-2 mr-10" onClick={toggleShowStatus}>เสร็จแล้ว</button>
-                  <button className="py-2 px-5 bg-red-600 hover:bg-red-400 text-white rounded-lg mr-10" onClick={toggleShowStatus}>ไม่มีเมนูนี้</button>
+                  <button
+                    className="py-2 px-5 bg-green-500 hover:bg-green-400 text-white rounded-lg mt-2 mr-10"
+                    onClick={() => finishedOrder(order.id, order.order)}
+                    //onClick={toggleShowStatus}
+                  >
+                    เสร็จแล้ว
+                  </button>
+                  <button
+                    className="py-2 px-5 bg-red-600 hover:bg-red-400 text-white rounded-lg mr-10"
+                    //onClick={toggleShowStatus}
+                    onClick={() => rejectOrder(order.id, order.order)}
+                  >
+                    ไม่มีเมนูนี้
+                  </button>
                 </td>
                 <td className="font-medium ">
                   <div className="show-status">
                     {showStatus === false ? (
-                      <span className="bg-green-200 text-green-900  rounded-md px-2"
-                      >เสร็จแล้ว</span>
+                      <span className="bg-green-200 text-green-900  rounded-md px-2">
+                        เสร็จแล้ว
+                      </span>
                     ) : (
-                      <span className="bg-red-200 text-red-900 rounded-md px-2"
-                      >ยกเลิกแล้ว</span>
+                      <span className="bg-red-200 text-red-900 rounded-md px-2">
+                        ยกเลิกแล้ว
+                      </span>
                     )}
                   </div>
                 </td>
